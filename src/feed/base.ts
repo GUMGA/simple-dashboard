552a97221c95ce1007514138f71a5e3705b7a2fe
Felipe Sabadini Facina <fsabadini@hotmail.com>
2017-10-03 21:20:33 -0300

    feed

---
+++ src/feed/base.ts
@@ -0,0 +1,27 @@
+import { Configuration }  from '../common/configuration';
+import { RecordSet } from '../common/interfaces';
+
+export abstract class BaseFeed {
+
+    protected feed: any;
+    private configuration : Configuration;
+    private recordset : RecordSet;
+    private element: HTMLElement;
+
+    constructor(element: HTMLElement, recordset: RecordSet, configuration: Configuration) {
+        this.element = element;
+        this.recordset = recordset;
+        this.configuration  = Object.assign({}, new Configuration(), configuration);
+        this.render();
+    }
+
+    protected abstract onInit():void;
+    protected abstract processRecordSet(recordset: RecordSet, configuration: Configuration):void;
+    protected abstract generateTemplate(element: HTMLElement, recordset: RecordSet, configuration: Configuration):void;
+
+    public render() :void {
+        this.onInit();
+        this.processRecordSet(this.recordset, this.configuration);
+        this.generateTemplate(this.element, this.recordset, this.configuration);
+    };
+}
